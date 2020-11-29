86948dd339b40e84b0e35aa53fe0d962316e5113
Dupuis Louise <louise.dupuis@student.ecp.fr>
2020-11-29 18:51:27 +0100

    clauses

--- sat_method.py
+++ sat_method.py
@@ -1,4 +1,4 @@
-from itertools import combinaison
+from itertools import combinations
 
 global unique_number
 unique_number = 1
@@ -64,4 +64,62 @@ def initiate_s(comparaison_list, J, H, N):
             result_dict[(j, h)] = unique_number
             unique_number += 1
     return result_dict
+
+
+def clause_3a(Y, comparaison_list, J, H, N ):
+    clause = []
+    for a,b in Y.keys():
+        clause.append([Y[(a,b)], Y[(b,a)]])
+    return clause
+
+def clause_3b(Y, comparaison_list, J, H, N):
+    clause = []
+    for a, b in Y.keys():
+        if set(a).issubset(set(b)):
+            clause.append([Y[(b, b)]])
+    return clause
+
+def clause_3c(Y, comparaison_list, J, H, N):
+    clause = []
+    sous_parties = []
+    for taille in range(N + 1):
+        combi = list(combinations(list(range(N)), taille))
+        sous_parties += combi
+
+    for a in sous_parties:
+        for b in sous_parties:
+            for c in sous_parties:
+                clause.append([ -Y[(a, b)], -Y[(b, c)], Y[(a, c)]])
+    return clause
+
+def clause_5a(Z, S,comparaison_list, J, H, N):
+    clauses = []
+    for j in range(J):
+        for h in range(H):
+            for h_prime in range(h + 1, H):
+                clauses.append([Z[j, h], -S[j, h_prime]])
+    return clauses
+
+def clause_5b(Z_prime, S,comparaison_list, J, H, N):
+    clauses = []
+    for j in range(J):
+        for h in range(H):
+            for h_prime in range(h + 1, H):
+                clauses.append([Z_prime[j, h], -S[j, h_prime]])
+    return clauses
+
+
+def clause_5c(Z_prime, S,comparaison_list, J, H, N):
+    clauses = []
+    for j in range(J):
+        for h in range(H):
+            clauses.append([ - Z_prime[j, h], -S[j, h]])
+    return clauses
+
+def clause_6( S, comparaison_list, J, H, N):
+    clauses = []
+    for j in range(J):
+        clauses.append([ S[j, h] for h in range(H)])
+    return clauses
+
 
