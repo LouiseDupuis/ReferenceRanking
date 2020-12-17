c0077caac791232c59f6ef6ff0afa4ff4c6e6ff3
Tyah Mohamed <mohamed.tyah@student.ecp.fr>
2020-12-17 12:18:19 +0100

    Reference points domination fix

--- SAT_RMP.py
+++ SAT_RMP.py
@@ -5,6 +5,7 @@ import random
 import pylgl
 
 
+
 class RMP:
     
     def __init__(self, reference_points, coalition_importance):
@@ -17,12 +18,24 @@ class RMP:
         self.reference_points = reference_points
         self.criteria = list(range(len(reference_points[0])))
         self.coalition_importance = coalition_importance
+        
+    def __str__(self):
+        # function to print a summary of a RMP model
+        print("============================ RMP Model ==========================")
+        print("Number of Reference Points : ", len(self.reference_points))
+        print()
+        i = 0
+        for ref in self.reference_points:
+            print(i, " ", ref)
+            i += 1
+        print()
+        return ''
 
     def is_more_important_coalition(self, coalition_1, coalition_2):
         """
             fonction de comparaison de domination de deux ensembles de critères
         """
-        print('coal_1', coalition_1, 'coal_2', coalition_2)
+        #print('coal_1', coalition_1, 'coal_2', coalition_2)
         return self.coalition_importance[(coalition_1, coalition_2)] > 0
     
     def dominant_criteria_set(self, a, reference_point):
@@ -34,13 +47,29 @@ class RMP:
             if a[criterion] >= reference_point[criterion]:
                 result.append(criterion)
         return tuple(sorted(result))
+
+    def domination(self):
+        for rh in self.reference_points:
+            for rh_prime in [el for el in self.reference_points if el != rh]:
+                index = 0
+                while rh[index] == rh_prime[index] and index < len(rh):
+                    index += 1
+                if rh[index] > rh_prime[index]:
+                    for i in range(index, len(rh)):
+                        if rh[i] < rh_prime[i]:
+                            return False
+                else:
+                    for i in range(index, len(rh)):
+                        if rh[i] > rh_prime[i]:
+                            return False
+        return True
     
     def compare(self, object_1, object_2):
         """
             returns a tuple ( is_preferred(object_1, object_2), is_indifferent(object_1, object_2))
         """
         for reference_point in self.reference_points:
-            print(reference_point)
+            #print(reference_point)
             coalition_1 =  self.dominant_criteria_set(object_1, reference_point)
             coalition_2 =  self.dominant_criteria_set(object_2, reference_point)
             if not self.is_more_important_coalition(coalition_2, coalition_1):
@@ -51,6 +80,8 @@ class RMP:
                 return False, False
         return False, True
     
+
+
 class SatRmp:
     
     instance_id = 1
@@ -309,30 +340,44 @@ class SatRmp:
         self.clauses = clauses
         return clauses
     
+    def adjust_intervals(self, interval_1, interval_2):
+        if interval_1[1] <  interval_2[0]:
+            raise Exception("Couldn't create RMP model")
+        elif interval_1[0] >= interval_2[1]:
+            return interval_1, interval_2
+        elif interval_1[1] <= interval_2[1]:
+            intersection = [max(interval_1[0], interval_2[0]), min(interval_1[1], interval_2[1])]
+            middle = intersection[0] + (intersection[1] - intersection[0])/2
+            return [middle, intersection[1]], [intersection[0], middle]
+        elif interval_2[1] <= interval_1[1]:
+            intersection = [max(interval_1[0], interval_2[0]), min(interval_1[1], interval_2[1])]
+            middle = intersection[0] + (intersection[1] - intersection[0])/2
+            return [middle, interval_1[1]], [interval_2[0], middle]    
+    
     def run_SAT(self):
         self.SAT_result = pylgl.solve(self.clauses)
     
     def create_RMP_model(self):
-        assert self.SAT_result != 'UNSAT', "Couldn't create RMP model"
-        SAT_X, SAT_Y = dict(), dict()
+        assert self.SAT_result != 'UNSAT', "Problem is UNSAT"
+        SAT_X, SAT_Y, SAT_D = dict(), dict(), dict()
 
-        # récupérer les x et les y donnés par le solveur SAT
+        # récupérer les x, y et les d donnés par le solveur SAT
         for variable in self.SAT_result:
             if abs(int(variable)) in self.X.inverse:
                 SAT_X[self.X.inverse[abs(int(variable))]] = int(variable)
             if abs(int(variable)) in self.Y.inverse:
                 SAT_Y[self.Y.inverse[abs(int(variable))]] = int(variable)
+            if abs(int(variable)) in self.D.inverse:
+                SAT_D[self.D.inverse[abs(int(variable))]] = int(variable)
 
         # contruire les profils de référence
         reference_points = []
-
+        
         for h in range(self.H):
-            reference_point = []
+            reference_point_interval = []
             for i in range(self.N):
                 min_value, max_value = 0, 1
                 Xi = [self.comparaison_list[el][0][i] for el in range(self.J)] + [self.comparaison_list[el][1][i] for el in range(self.J)]
-                # on trie les K par ordre croissant :
-                # Xi.sort() pas necessaire ?
                 for k in Xi:
                     if SAT_X[i, h, k] > 0:
                         max_value = min(max_value, k)
@@ -340,13 +385,30 @@ class SatRmp:
                         min_value = max(min_value, k)
                 if min_value >= max_value:
                     raise Exception("Couldn't create RMP model")
-                criterion = random.uniform(min_value, max_value)
-                while not criterion > min_value:
-                    criterion = random.uniform(min_value, max_value)
-                reference_point.append(criterion)
-            reference_points.append(reference_point) 
+                # construction des intervals de valeurs possible pour les points de references
+                reference_point_interval.append([min_value, max_value])
+            reference_points.append(reference_point_interval)
+            
+        # Ajustement des intervals pour respecter la dominance entre points de refenrences
+        for d in SAT_D:
+            if SAT_D[d] > 0:
+                rh = reference_points[d[0]]
+                rh_prime = reference_points[d[1]]
+                for i in range(len(rh)):
+                    tmp = self.adjust_intervals(rh[i], rh_prime[i])
+                    rh[i], rh_prime[i] = tmp
+                    
+        # Choix des valeurs des critères dans les intervals ajustés
+        for reference_point in reference_points:
+            for i in range(len(reference_point)):
+                criterion = random.uniform(reference_point[i][0], reference_point[i][1])
+                while not criterion > reference_point[i][0]:
+                    criterion = random.uniform(reference_point[i][0], rh[i][1])
+                reference_point[i] = criterion
             
         self.RMP_model = RMP(reference_points, SAT_Y)
+
+
     
 def generate_random_RMP_learning_set(J, N):
     result = []
@@ -363,22 +425,39 @@ def test_rmp(J, N, H):
     try:
         sat_rmp.create_RMP_model()
     except AssertionError:
-        print("Problem is not solvable (UNSAT)")
         return False, 'UNSAT'
     except Exception:
-        print("Problem while creating RMP")
         return False, 'RMP'
     for j in range(J):
         if not sat_rmp.RMP_model.compare(comparaison_list[j][0], comparaison_list[j][1])[0]:
             return False, 'Restitution'
-    return True, 'All good'
+    if not sat_rmp.RMP_model.domination():
+        return False, 'Dominance'
+    return True, sat_rmp
+
+def test_multiple_rmp_stats(iterations, J, N, H):
+    stats = {'UNSAT': 0, 'RMP': 0, 'Restitution': 0, 'Dominance': 0, 'Success': 0}
+    success_models = []
+    for _ in range(iterations):
+        test_result = test_rmp(J, N, H)
+        if test_result[0]:
+            stats['Success'] += 1
+            success_models.append(test_result[1])
+        else:
+            stats[test_result[1]] += 1
+    print()
+    return stats, success_models
     
     
 if __name__ == '__main__':
     """
     J = 10
-    N = 5
+    N = 3
     H = 2
-    test_rmp(J, N, H)
+    iterations = 5
+    stats = test_multiple_rmp_stats(iterations, J, N, H)
+    print(stats[0], '\n')
+    for success in stats[1]:
+        print(success.RMP_model)
     """
 
